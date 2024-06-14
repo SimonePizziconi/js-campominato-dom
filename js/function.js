e29e1daa40374fb7fcd7f41710b7f4b39591e2b2
SimonePizziconi <simonepizziconi05@gmail.com>
2024-06-14 15:47:11 +0200

    feat: create function in file fuction.js

--- js/function.js
+++ js/function.js
@@ -4,3 +4,25 @@ function createElementWithClass (tag, cssClass) {
     element.classList.add(cssClass);
     return element
 }
+
+// CREA NUMERO CASUALE IN UN RANGE
+function randomNumberRange (min, max){
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+// GENERA NUMERI CASUALI IN UN RANGE DI NUMERI
+function randomNumberGenerationRange (min, max){
+    // Crea array vuoto
+    const numberList = [];
+
+    // Crea un ciclo che genera 16 numeri casuali
+    while (numberList.length < max){
+        let randomNumber = randomNumberRange (min, max);
+
+        // inserisci il numero casuale solo se non è gia presente nell'array
+        if(!numberList.includes(randomNumber)){
+            numberList.push(randomNumber);
+        }
+    }
+    return numberList
+}
