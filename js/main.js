bafb9c4de60ff55126eca1d340b2215f193ab005
SimonePizziconi <simonepizziconi05@gmail.com>
2024-06-14 16:39:57 +0200

    feat: add condition for print message win or lose

--- js/main.js
+++ js/main.js
@@ -5,7 +5,6 @@ const containerGrid = document.querySelector(".grid-container");
 // prendiamo il bottone da HTML
 const playButton = document.querySelector("button");
 
-
 // crea un evento al click del bottone
 playButton.addEventListener("click", 
     function(){
@@ -15,23 +14,40 @@ playButton.addEventListener("click",
         // Genera 16 numeri casuali
         const listBomb = randomNumberGenerationRange(1, 100, 16);
         console.log(listBomb);
+
+        // Crea un numero di celle sicure sottraendo box sicuri - box bomba
+        let totalBox = 100;
+        let boxBomb = listBomb.length;
+        let safeBox = totalBox - boxBomb;
+        let clickBoxSafe = 0;
     
         // Ogni cella ha un numero progressivo, da 1 a 100.
-        for (let i = 1; i <= 100; i++){
+        for (let i = 1; i <= totalBox; i++){
         let square = createElementWithClass("div", "box");
         square.textContent = i;
         containerGrid.append(square);
-
+        
             // Quando l’utente clicca su ogni cella se c'é la boma diventa rosso senno bianco 
             square.addEventListener("click", 
                 function (){
                 if (listBomb.includes(i)) {
+                    // Aggiungi classe BOMBA
                     this.classList.add("bomb")
-                } else{
-                    this.classList.toggle("active");
+
+                    // Manda messaggio che hai perso
+                    const messageLose = document.getElementById("lose-or-win").innerHTML = "HAI PERSO..."
+                } else {
+                    // Aggiugni classe SALVO
+                    this.classList.add("active");
+
+                    // Incrementa valore box sicuri
+                    clickBoxSafe++;
 
                     // ed emetti un messaggio in console con il numero della cella cliccata.
                     console.log(`hai cliccato la cella con il numero ${i}`);
+                } if (clickBoxSafe === safeBox){
+                    // Manda messaggio che hai vinto
+                   const messageWin = document.getElementById("lose-or-win").innerHTML = "HAI VINTO"
                 }
                 }
             );
